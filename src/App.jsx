7b5266178d3ac2715e990257d067a483fdb5a08a
lau-ifma <lauandersoncosta@acad.ifma.edu.br>
2024-09-18 23:45:46 -0300

    added modalEditUser

--- src/App.jsx
+++ src/App.jsx
@@ -26,20 +26,24 @@ function App() {
   }
 
   const [modal, setModal] = useState(false);
-
   const OpenModal = () => {
     setModal(!modal);
   };
-
   const CloseModal= () => {
     setModal(false);
   };
 
+  const [modalEdit, setModalEdit] = useState(false);
+  const OpenModalEdit = () => {
+    setModalEdit(!modalEdit);
+  };
+  const CloseModalEdit= () => {
+    setModalEdit(false);
+  };
+
   return (
     <>
-
       <h1 className='text-2xl text-center font-semibold flex justify-center max-md:text-xl'>Gerenciamento de Usuários</h1>
-
       <div className='my-0 mx-auto mt-2 bg-gray-300 w-[600px] rounded-md flex-col items-center p-3 max-md:w-[95%]'>
         <div className='flex justify-end mb-2'>
           <button
@@ -61,17 +65,6 @@ function App() {
           </thead>
 
           <tbody className='bg-gray-100 text-center'>
-          <tr className="h-[40px] border-b-2 border-gray-500">
-            <td>1</td>
-            <td>Raimundo</td>
-            <td>Vigia</td>
-            <td className="bg-gray-200">
-               <FaEdit className="text-orange-500 text-xl hover:text-orange-700 ml-[45%]"/>
-            </td>
-            <td className="bg-gray-200">
-               <MdDelete className="text-red-500 text-xl hover:text-red-700 ml-[45%]"/>
-            </td>
-          </tr>
 
             {users.map(user => (
               <tr key={user.id} className="h-[40px] border-b-2 border-gray-500">
@@ -79,7 +72,7 @@ function App() {
                 <td>{user.name}</td>
                 <td>{user.type}</td>
                 <td className="bg-gray-200">
-                  <FaEdit className="text-orange-500 text-xl hover:text-orange-700 ml-[45%]" />
+                  <FaEdit onClick={OpenModalEdit} className="text-orange-500 text-xl hover:text-orange-700 ml-[45%]" />
                 </td>
                 <td className="bg-gray-200">
                   <MdDelete onClick={() => deleteUser(user.id)} className="text-red-500 text-xl hover:text-red-700 ml-[45%]" />
@@ -89,6 +82,39 @@ function App() {
           </tbody>
         </table>
       </div>
+
+      {modalEdit && (
+        <div className="overlay fixed inset-0 bg-gray-800 bg-opacity-75 flex justify-center items-center">
+          <div className="bg-white p-5 rounded-lg w-[400px] max-md:w-[95%]">
+            <h2 className="text-xl mb-4 text-center font-semibold">Editar Usuário</h2>
+            <form >
+              <div className="mb-4">
+                <label className="block text-gray-700 font-medium">Novo nome:</label>
+                <input type="text" name="name" id="name" required
+                  className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring focus:ring-emerald-300"
+                />
+              </div>
+              <div className="mb-4">
+                  <label className="block text-gray-700 font-medium">Novo tipo:</label>
+                  <input type="text" name="type" id="type" required
+                  className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring focus:ring-emerald-300"
+                  />
+              </div>
+              <div className="flex justify-between">
+                  <button type="button" onClick={CloseModalEdit}
+                    className="bg-gray-500 hover:bg-gray-700 text-white px-4 py-2 rounded-md font-semibold"
+                  >Cancelar
+                  </button>
+
+                  <button type="submit" onClick={postAPI}
+                    className="bg-emerald-500 hover:bg-emerald-700 text-white px-4 py-2 rounded-md font-semibold"
+                    >Atualizar
+                  </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      )}
 
       {modal && (
         <div className="overlay fixed inset-0 bg-gray-800 bg-opacity-75 flex justify-center items-center">
